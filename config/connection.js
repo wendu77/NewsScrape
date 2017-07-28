1f614d1c64ddad46dc933e91639d9f1e4122c5be
Chinwendu Opara <wendusartandco@gmail.com>
2017-07-28 11:11:11 -0400

    trying to push to heroku again

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var mongoose = require("mongoose");
 // });
 
 //db config -- REPLACE DB_USERNAME/DB_PASSWORD/DATABASE_NAME WITH YOUR OWN FROM MLAB!
-var mongoDB = 'mongodb://wendu:7777Wen@ds127063.mlab.com:27063/goodnews';
+var mongoDB = 'mongodb://wendu:7777wen@ds127063.mlab.com:27063/goodnews';
 mongoose.connect(mongoDB, { useMongoClient: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
