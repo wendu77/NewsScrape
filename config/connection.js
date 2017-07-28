cc15ba8a5f37c2ed01ce4b4eb424426a49278d20
Chinwendu Opara <wendusartandco@gmail.com>
2017-07-28 11:05:48 -0400

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,13 @@
 var mongoose = require("mongoose");
 
 // Local Database Configuration with Mongoose
-mongoose.connect("mongodb://localhost/goodnews", function(error)
-	{if(error) throw error;
-	console.log("Database connected");
-});
+// mongoose.connect("mongodb://localhost/goodnews", function(error)
+// 	{if(error) throw error;
+// 	console.log("Database connected");
+// });
+
+//db config -- REPLACE DB_USERNAME/DB_PASSWORD/DATABASE_NAME WITH YOUR OWN FROM MLAB!
+var mongoDB = 'mongodb://wendu:7777Wen@ds127063.mlab.com:27063/goodnews';
+mongoose.connect(mongoDB, { useMongoClient: true })
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
